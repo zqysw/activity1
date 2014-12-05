c8762c00de659b01e34ae5ff07b5019fb76dbad1
zqysw <915040607@qq.com>
2014-12-05 15:00:18 +0800

    update 6

--- application/views/create_activity.php
+++ application/views/create_activity.php
@@ -4,7 +4,7 @@
 	<meta charset = "utf-8"> 
 	<meta http-equiv = "X-UA-Compatible" content = "IE=edge">
 	<meta name = "viewport" content = "width=device-width,initial-scale = 1">
-	<title>活动推荐</title>
+	<title>活动发布</title>
 	<link href = "/activity/style/bootstrap/css/bootstrap-theme.min.css" rel = "stylesheet">
 	<link href = "/activity/style/bootstrap/css/bootstrap.min.css" rel = "stylesheet">
 	<link href = "/activity/style/create/css/create_activity.css" type = "text/css" rel = "stylesheet"></link>
@@ -13,6 +13,8 @@
 	<script	src = "/activity/style/create/js/create_activity.js"></script>
 </head>
 <body>
-	
+	<div class="container">
+		<div class = "row" id = "top">
+				<div class = "col-xs-12" id = "top_font">活动发布</div>
 </body>
 </html>
