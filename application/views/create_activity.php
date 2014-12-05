a980ce41601fd3761d919ce83ec1cb39960520a1
zqysw <915040607@qq.com>
2014-12-05 17:52:24 +0800

    update 8

--- application/views/create_activity.php
+++ application/views/create_activity.php
@@ -4,7 +4,7 @@
 	<meta charset = "utf-8"> 
 	<meta http-equiv = "X-UA-Compatible" content = "IE=edge">
 	<meta name = "viewport" content = "width=device-width,initial-scale = 1">
-	<title>活动推荐</title>
+	<title>活动发布</title>
 	<link href = "/activity/style/bootstrap/css/bootstrap-theme.min.css" rel = "stylesheet">
 	<link href = "/activity/style/bootstrap/css/bootstrap.min.css" rel = "stylesheet">
 	<link href = "/activity/style/create/css/create_activity.css" type = "text/css" rel = "stylesheet"></link>
@@ -13,6 +13,240 @@
 	<script	src = "/activity/style/create/js/create_activity.js"></script>
 </head>
 <body>
-	
+	<div class="container">
+
+		<form class="form-horizontal" role="form">
+	  		<div class="form-group">		
+				<div id = "top">
+					<div class = "col-xs-12" id = "top_font">活动发布</div>
+				</div>
+			</div>
+			<div class = "form-group">
+				<div class = "col-xs-5 control-label">活动分类：</div>
+				<div class = "col-xs-7">
+					<select class = "form-control">
+						<option>科技</option>
+						<option>娱乐</option>
+						<option>生活</option>
+						<option>其他</option>
+					</select>
+				</div>
+			</div>
+			<div class = "form-group">
+				<div class = "col-xs-5 control-label">活动名称：</div>
+				<div class = "col-xs-7" >
+					<textarea  name = "act_name" class = "form-control" placeholder = "简称" rows ="1"></textarea>
+				</div>
+			</div>
+			<div class = "form-group">
+				<div class = "col-xs-5 control-label">活动地点：</div>
+				<div class = "col-xs-7">
+					<textarea name = "act_location" class = "form-control" rows = "1"></textarea>
+				</div>
+				<div class = "col-xs-6">
+					 <button class = "btn btn-primary btn-sm">地图定位</button>
+				</div>
+			</div>
+			<div class = "form-group">
+				<div class = "col-xs-5 control-label">活动时间：</div>
+				<div class = "col-xs-12">
+					<select >
+						<option>14</option>
+						<option>15</option>
+						<option>16</option>
+						<option>17</option>
+					</select>
+				年
+					<select >
+						<option>1</option>
+						<option>2</option>
+						<option>3</option>
+						<option>4</option>
+						<option>5</option>
+						<option>6</option>
+						<option>7</option>
+						<option>8</option>
+						<option>9</option>
+						<option>10</option>
+						<option>11</option>
+						<option>12</option>
+					</select>	
+				月
+					<select >
+						<option>1</option>
+						<option>2</option>
+						<option>3</option>
+						<option>4</option>
+						<option>5</option>
+						<option>6</option>
+						<option>7</option>
+						<option>8</option>
+						<option>9</option>
+						<option>10</option>
+						<option>11</option>
+						<option>12</option>	
+						<option>13</option>
+						<option>14</option>
+						<option>15</option>
+						<option>16</option>
+						<option>17</option>
+						<option>18</option>
+						<option>19</option>
+						<option>20</option>
+						<option>21</option>
+						<option>22</option>
+						<option>23</option>
+						<option>24</option>	
+						<option>25</option>
+						<option>26</option>
+						<option>27</option>
+						<option>28</option>
+						<option>29</option>
+						<option>30</option>
+						<option>31</option>		
+					</select>	
+				日	
+					<select >
+						<option>1</option>
+						<option>2</option>
+						<option>3</option>
+						<option>4</option>
+						<option>5</option>
+						<option>6</option>
+						<option>7</option>
+						<option>8</option>
+						<option>9</option>
+						<option>10</option>
+						<option>11</option>
+						<option>12</option>	
+						<option>13</option>
+						<option>14</option>
+						<option>15</option>
+						<option>16</option>
+						<option>17</option>
+						<option>18</option>
+						<option>19</option>
+						<option>20</option>
+						<option>21</option>
+						<option>22</option>
+						<option>23</option>
+						<option>24</option>	
+					</select>
+				时--
+				</div>
+				<div class = "col-xs-12">
+					<select >
+						<option>14</option>
+						<option>15</option>
+						<option>16</option>
+						<option>17</option>
+					</select>
+				年
+					<select >
+						<option>1</option>
+						<option>2</option>
+						<option>3</option>
+						<option>4</option>
+						<option>5</option>
+						<option>6</option>
+						<option>7</option>
+						<option>8</option>
+						<option>9</option>
+						<option>10</option>
+						<option>11</option>
+						<option>12</option>
+					</select>	
+				月
+					<select >
+						<option>1</option>
+						<option>2</option>
+						<option>3</option>
+						<option>4</option>
+						<option>5</option>
+						<option>6</option>
+						<option>7</option>
+						<option>8</option>
+						<option>9</option>
+						<option>10</option>
+						<option>11</option>
+						<option>12</option>	
+						<option>13</option>
+						<option>14</option>
+						<option>15</option>
+						<option>16</option>
+						<option>17</option>
+						<option>18</option>
+						<option>19</option>
+						<option>20</option>
+						<option>21</option>
+						<option>22</option>
+						<option>23</option>
+						<option>24</option>	
+						<option>25</option>
+						<option>26</option>
+						<option>27</option>
+						<option>28</option>
+						<option>29</option>
+						<option>30</option>
+						<option>31</option>		
+					</select>	
+				日	
+					<select >
+						<option>1</option>
+						<option>2</option>
+						<option>3</option>
+						<option>4</option>
+						<option>5</option>
+						<option>6</option>
+						<option>7</option>
+						<option>8</option>
+						<option>9</option>
+						<option>10</option>
+						<option>11</option>
+						<option>12</option>	
+						<option>13</option>
+						<option>14</option>
+						<option>15</option>
+						<option>16</option>
+						<option>17</option>
+						<option>18</option>
+						<option>19</option>
+						<option>20</option>
+						<option>21</option>
+						<option>22</option>
+						<option>23</option>
+						<option>24</option>	
+					</select>
+				时
+				</div>
+			</div>
+			<div class = "form-group">
+				<div class = "col-xs-5 control-label">联系方式：</div>
+				<div class = "col-xs-5">
+					<input name = "act_phone" class = "form-control" style = "width:145px;" placeholder = "必填">
+				</div>
+				<div class = "col-xs-2">
+					<span style = "padding-top:9px;" class = "glyphicon glyphicon-earphone"></span>
+				</div>
+			</div>
+			<div class = "form-group">
+				<div class = "col-xs-5 control-label">活动介绍：</div>
+				<div class = "col-xs-12">
+					<textarea name = "act_introduce" class = "form-control" rows = "2"></textarea>
+				</div>
+			</div>
+			<div class = "form-group">
+				<div class = "col-xs-12">
+					<button style = " margin-left:180px;margin-top:10px;" class = "btn btn-primary">发布</button>
+				</div>
+			</div>
+		</form>
+		<div class = "row bottom">
+			<div class = "col-xs-3"><a href = "http://localhost/activity/index.php?/index/index" class = "btn btn-primary btn-sm " role = "button">活动推荐</a></div>
+			<div class = "col-xs-3"><a href = "#" class = "btn btn-primary btn-sm disabled" role = "button">活动发布</a></div>
+			<div class = "col-xs-3"><a href = "#" class = "btn btn-primary btn-sm" role = "button">活动搜索</a></div>
+			<div class = "col-xs-3"><a href = "#" class = "btn btn-primary btn-sm" role = "button">我的世界</a></div>
+		</div>
+	</div>
 </body>
 </html>
